aa0fdb8b792761fdd3a42cfcc15e07dfafa02720
Abimbola Idowu <abimbola2010@gmail.com>
2016-05-08 00:23:54 +0100

    Add hooks

--- routes/countly-hook.js
+++ routes/countly-hook.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/listener', function(req, res, next) {
+router.post('/platform/ios', function(req, res, next) {
   console.log('abimbola', req.body)
+  res.end();
+});
+router.post('/session/count', function(req, res, next) {
+  console.log('session', req.body)
   res.end();
 });
 
